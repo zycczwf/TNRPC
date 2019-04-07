3b7c827ea0e15addc4850d9f05361e86c9d64ef4
admin <zycczwf@sina.com>
2019-04-07 15:14:30 +0800

    add gongdian

--- TNRPC/FormMain.Designer.cs
+++ TNRPC/FormMain.Designer.cs
@@ -252,8 +252,28 @@
             this.label381003 = new System.Windows.Forms.Label();
             this.label381002 = new System.Windows.Forms.Label();
             this.label381001 = new System.Windows.Forms.Label();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.panel13 = new System.Windows.Forms.Panel();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox56 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage9.SuspendLayout();
             this.groupBox57.SuspendLayout();
@@ -319,7 +339,16 @@
             this.groupBox3.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel13.SuspendLayout();
+            this.panel12.SuspendLayout();
+            this.panel11.SuspendLayout();
+            this.panel10.SuspendLayout();
+            this.panel9.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel7.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox56.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -327,6 +356,7 @@
             this.tabControl1.Controls.Add(this.tabPage9);
             this.tabControl1.Controls.Add(this.tabPage7);
             this.tabControl1.Controls.Add(this.tabPage8);
+            this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Font = new System.Drawing.Font("仿宋", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
@@ -336,6 +366,7 @@
             // 
             // tabPage9
             // 
+            this.tabPage9.Controls.Add(this.groupBox56);
             this.tabPage9.Controls.Add(this.groupBox57);
             this.tabPage9.Controls.Add(this.groupBox63);
             this.tabPage9.Location = new System.Drawing.Point(4, 26);
@@ -354,14 +385,14 @@
             this.groupBox57.Controls.Add(this.label13);
             this.groupBox57.Location = new System.Drawing.Point(6, 6);
             this.groupBox57.Name = "groupBox57";
-            this.groupBox57.Size = new System.Drawing.Size(558, 170);
+            this.groupBox57.Size = new System.Drawing.Size(558, 158);
             this.groupBox57.TabIndex = 1;
             this.groupBox57.TabStop = false;
             this.groupBox57.Text = "装配";
             // 
             // textBox13
             // 
-            this.textBox13.Location = new System.Drawing.Point(284, 41);
+            this.textBox13.Location = new System.Drawing.Point(284, 29);
             this.textBox13.Multiline = true;
             this.textBox13.Name = "textBox13";
             this.textBox13.ReadOnly = true;
@@ -371,7 +402,7 @@
             // label14
             // 
             this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(413, 22);
+            this.label14.Location = new System.Drawing.Point(413, 10);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(44, 16);
             this.label14.TabIndex = 1;
@@ -379,7 +410,7 @@
             // 
             // textBox14
             // 
-            this.textBox14.Location = new System.Drawing.Point(6, 41);
+            this.textBox14.Location = new System.Drawing.Point(6, 29);
             this.textBox14.Multiline = true;
             this.textBox14.Name = "textBox14";
             this.textBox14.ReadOnly = true;
@@ -389,7 +420,7 @@
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(105, 22);
+            this.label13.Location = new System.Drawing.Point(105, 10);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(44, 16);
             this.label13.TabIndex = 0;
@@ -403,14 +434,14 @@
             this.groupBox63.Controls.Add(this.label15);
             this.groupBox63.Location = new System.Drawing.Point(592, 6);
             this.groupBox63.Name = "groupBox63";
-            this.groupBox63.Size = new System.Drawing.Size(558, 170);
+            this.groupBox63.Size = new System.Drawing.Size(558, 158);
             this.groupBox63.TabIndex = 1;
             this.groupBox63.TabStop = false;
             this.groupBox63.Text = "充电";
             // 
             // textBox15
             // 
-            this.textBox15.Location = new System.Drawing.Point(285, 41);
+            this.textBox15.Location = new System.Drawing.Point(285, 29);
             this.textBox15.Multiline = true;
             this.textBox15.Name = "textBox15";
             this.textBox15.ReadOnly = true;
@@ -419,7 +450,7 @@
             // 
             // textBox16
             // 
-            this.textBox16.Location = new System.Drawing.Point(6, 41);
+            this.textBox16.Location = new System.Drawing.Point(6, 29);
             this.textBox16.Multiline = true;
             this.textBox16.Name = "textBox16";
             this.textBox16.ReadOnly = true;
@@ -429,7 +460,7 @@
             // label16
             // 
             this.label16.AutoSize = true;
-            this.label16.Location = new System.Drawing.Point(400, 22);
+            this.label16.Location = new System.Drawing.Point(400, 10);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(44, 16);
             this.label16.TabIndex = 1;
@@ -438,7 +469,7 @@
             // label15
             // 
             this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(108, 22);
+            this.label15.Location = new System.Drawing.Point(108, 10);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(44, 16);
             this.label15.TabIndex = 0;
@@ -2731,6 +2762,85 @@
             this.label381001.TabIndex = 0;
             this.label381001.Text = "N/A";
             // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.panel13);
+            this.tabPage1.Controls.Add(this.panel12);
+            this.tabPage1.Controls.Add(this.panel11);
+            this.tabPage1.Controls.Add(this.panel10);
+            this.tabPage1.Controls.Add(this.panel9);
+            this.tabPage1.Controls.Add(this.panel8);
+            this.tabPage1.Controls.Add(this.panel7);
+            this.tabPage1.Location = new System.Drawing.Point(4, 26);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(1156, 707);
+            this.tabPage1.TabIndex = 11;
+            this.tabPage1.Text = "供电";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // panel13
+            // 
+            this.panel13.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel13.Controls.Add(this.label4);
+            this.panel13.Location = new System.Drawing.Point(0, 611);
+            this.panel13.Name = "panel13";
+            this.panel13.Size = new System.Drawing.Size(1150, 96);
+            this.panel13.TabIndex = 6;
+            // 
+            // panel12
+            // 
+            this.panel12.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel12.Controls.Add(this.label7);
+            this.panel12.Location = new System.Drawing.Point(0, 518);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(1150, 96);
+            this.panel12.TabIndex = 5;
+            // 
+            // panel11
+            // 
+            this.panel11.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel11.Controls.Add(this.label6);
+            this.panel11.Location = new System.Drawing.Point(0, 416);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(1150, 96);
+            this.panel11.TabIndex = 4;
+            // 
+            // panel10
+            // 
+            this.panel10.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel10.Controls.Add(this.label5);
+            this.panel10.Location = new System.Drawing.Point(0, 314);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(1150, 96);
+            this.panel10.TabIndex = 3;
+            // 
+            // panel9
+            // 
+            this.panel9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel9.Controls.Add(this.label3);
+            this.panel9.Location = new System.Drawing.Point(0, 212);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(1150, 96);
+            this.panel9.TabIndex = 2;
+            // 
+            // panel8
+            // 
+            this.panel8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel8.Controls.Add(this.label2);
+            this.panel8.Location = new System.Drawing.Point(0, 110);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(1150, 96);
+            this.panel8.TabIndex = 1;
+            // 
+            // panel7
+            // 
+            this.panel7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel7.Controls.Add(this.label1);
+            this.panel7.Location = new System.Drawing.Point(0, 8);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(1150, 96);
+            this.panel7.TabIndex = 0;
+            // 
             // pictureBox1
             // 
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
@@ -2746,6 +2856,118 @@
             this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
             this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
             this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "充电A区";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "充电B区";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "充电C区";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 16);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "球磨车间";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 16);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "浇铸";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 39);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 16);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "合膏车间";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "装配";
+            // 
+            // groupBox56
+            // 
+            this.groupBox56.Controls.Add(this.label9);
+            this.groupBox56.Controls.Add(this.label8);
+            this.groupBox56.Controls.Add(this.textBox2);
+            this.groupBox56.Controls.Add(this.textBox1);
+            this.groupBox56.Location = new System.Drawing.Point(6, 172);
+            this.groupBox56.Name = "groupBox56";
+            this.groupBox56.Size = new System.Drawing.Size(558, 161);
+            this.groupBox56.TabIndex = 2;
+            this.groupBox56.TabStop = false;
+            this.groupBox56.Text = "供电";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(267, 123);
+            this.textBox1.TabIndex = 16;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(284, 29);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(267, 123);
+            this.textBox2.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(105, 10);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 16);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Send";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(413, 10);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 16);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Recv";
             // 
             // FormMain
             // 
@@ -2875,7 +3097,24 @@
             this.groupBox2.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.panel13.ResumeLayout(false);
+            this.panel13.PerformLayout();
+            this.panel12.ResumeLayout(false);
+            this.panel12.PerformLayout();
+            this.panel11.ResumeLayout(false);
+            this.panel11.PerformLayout();
+            this.panel10.ResumeLayout(false);
+            this.panel10.PerformLayout();
+            this.panel9.ResumeLayout(false);
+            this.panel9.PerformLayout();
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox56.ResumeLayout(false);
+            this.groupBox56.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -3106,5 +3345,25 @@
         private System.Windows.Forms.TextBox textBox14;
         private System.Windows.Forms.TextBox textBox15;
         private System.Windows.Forms.TextBox textBox16;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel13;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox56;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- TNRPC/FormMain.cs
+++ TNRPC/FormMain.cs
@@ -21,13 +21,27 @@ namespace TNRPC {
         }
 
         private void FormMain_Shown(object sender, EventArgs e) {
-            string used = ConfigurationManager.AppSettings["USED"];
-            string[] coms = used.Split(',');
-            foreach (string com in coms) {
-                Thread worker = new Thread(new ParameterizedThreadStart(Transceiver));
-                //随主线程退出而退出
-                worker.IsBackground = true;
-                worker.Start(ConfigurationManager.AppSettings[com]);
+            //温度
+            string used = ConfigurationManager.AppSettings["WENDU"];
+            if (used != null && used.Length > 0) {
+                string[] coms = used.Split(',');
+                foreach (string com in coms) {
+                    Thread worker = new Thread(new ParameterizedThreadStart(WenDu));
+                    //随主线程退出而退出
+                    worker.IsBackground = true;
+                    worker.Start(ConfigurationManager.AppSettings[com]);
+                }
+            }
+            //电度
+            used = ConfigurationManager.AppSettings["DIANDU"];
+            if (used != null && used.Length > 0) {
+                string[] coms = used.Split(',');
+                foreach (string com in coms) {
+                    Thread worker = new Thread(new ParameterizedThreadStart(DianDu));
+                    //随主线程退出而退出
+                    worker.IsBackground = true;
+                    worker.Start(ConfigurationManager.AppSettings[com]);
+                }
             }
         }
 
@@ -36,7 +50,8 @@ namespace TNRPC {
             GZWD //干燥温度
         }
 
-        private void Transceiver(Object com) {
+        //温度
+        private void WenDu(Object com) {
             string[] parameters = com.ToString().Split(',');
             //根据参数判断是什么工艺参数
             string process = null;
@@ -53,6 +68,8 @@ namespace TNRPC {
                     equipmentTypeID = "3";
                     break;
                 case "COM9":
+                case "COM10":
+                case "COM11":
                     process = PROCESS.GZWD.ToString();
                     paramID = "40001";
                     equipmentTypeID = "4";
@@ -108,6 +125,108 @@ namespace TNRPC {
                             if (process.Equals(PROCESS.GZWD.ToString())) {
                                 SetText("textBox13", parameters[0] + "/" + DateTime.Now.Hour + ":" + DateTime.Now.Minute + "<=" + ((bufferR is null) ? "N/A" : SoftBasic.ByteToHexString(bufferR)) + "\n");
                             }
+
+                            //////////////////////////////////////数据解析，在界面上显示结果，并存储到数据库
+                            string showResult = null;//界面显示的结果
+                            if (bufferR is null) {
+                                showResult = "N/A";//设备不可用
+                            } else if (!SoftCRC16.CheckCRC16(bufferR)) {
+                                showResult = "ERROR";//返回数据错误
+                            } else {
+                                //解析数据
+                                ReverseBytesTransform transform = new ReverseBytesTransform();//数据转换工具
+                                float data = (float)transform.TransInt16(bufferR, 3) / 10;
+                                //水温值超出正常范围
+                                if (data > 100 || data < 0) {
+                                    showResult = "ERROR";
+                                } else {
+                                    showResult = data.ToString("f1") + "℃";//显示温度值
+
+                                    //保存到数据库
+                                    using (MySqlConnection conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["MYSQL"].ConnectionString)) {
+                                        //打开数据库连接
+                                        conn.Open();
+
+                                        //判断当前值是否超上限或超下限
+                                        string status = "2";//2表示在范围之内
+                                        using (MySqlCommand cmd = new MySqlCommand("select max, min FROM tb_parameterinfo where id = '" + paramID + "'", conn)) {
+                                            using (MySqlDataReader reader = cmd.ExecuteReader()) {
+                                                if (reader.Read()) {
+                                                    if (data > reader.GetFloat("max")) {
+                                                        status = "3";
+                                                    } else if (data < reader.GetFloat("min")) {
+                                                        status = "1";
+                                                    }
+                                                }
+                                            }
+
+                                            //准备插入一条数据
+                                            cmd.CommandText = "insert into tb_equipmentparamrecord (id,equipmentid,paramID,recordTime,value,recorder,equipmentTypeID,status) values('" + Guid.NewGuid().ToString("N") + "','" + equipmentID + "','" + paramID + "','" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "','" + data + "','仪表采集','" + equipmentTypeID + "','" + status + "')"; ;
+                                            cmd.ExecuteNonQuery();
+                                        }
+                                    }
+                                }
+                            }
+                            //设置界面
+                            SetText("label" + equipmentID, showResult);
+                        } catch (Exception e) {
+                            Console.WriteLine(e.Message);
+                        }
+                    }
+                    //间隔5分钟采集一次数据
+                    Thread.Sleep(300000);
+                } catch (Exception e) {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        //电度
+        private void DianDu(Object com) {
+            //工艺参数
+            string paramID = "60001";
+            string equipmentTypeID = "6";
+
+            string[] parameters = com.ToString().Split(',');
+            SerialPort serialPort = new SerialPort();
+            serialPort.PortName = parameters[0];
+            serialPort.BaudRate = Convert.ToInt32(parameters[1]);
+            serialPort.DataBits = Convert.ToInt32(parameters[2]);
+            serialPort.Parity = (Parity)Convert.ToInt32(parameters[3]);
+            serialPort.StopBits = (StopBits)Convert.ToInt32(parameters[4]);
+
+            int startNo = Convert.ToInt32(parameters[5]);
+            int num = Convert.ToInt32(parameters[6]);
+
+            while (true) {
+                //保证循环不退出
+                try {
+                    if (!serialPort.IsOpen) {
+                        serialPort.Open();
+                    }
+                    for (int i = 1; i <= num; i++) {
+                        try {
+                            //设备ID，数据库中设置好的数值
+                            int equipmentID = startNo + i;
+
+                            /////////////////////////////////////////发送数据（modbusRTU协议）
+                            string orderWithoutCrc = string.Format("{0:X2}", i) + "0310010001";
+                            byte[] bufferS = SoftCRC16.CRC16(SoftBasic.HexStringToBytes(orderWithoutCrc));
+                            serialPort.Write(bufferS, 0, bufferS.Length);
+                            //将发送的数据显示在窗体相应的位置
+                            SetText("textBox1", parameters[0] + "/" + DateTime.Now.Hour + ":" + DateTime.Now.Minute + "=>" + SoftBasic.ByteToHexString(bufferS) + "\n");
+
+                            //////////////////////////////////////////等待数据
+                            Thread.Sleep(500);
+
+                            /////////////////////////////////////////接收数据。
+                            byte[] bufferR = null;
+                            if (serialPort.BytesToRead > 0) {
+                                bufferR = new byte[serialPort.BytesToRead];
+                                serialPort.Read(bufferR, 0, bufferR.Length);
+                            }
+                            //将接收的数据显示在窗体相应的位置，没有回应数据显示N/A。
+                            SetText("textBox13", parameters[0] + "/" + DateTime.Now.Hour + ":" + DateTime.Now.Minute + "<=" + ((bufferR is null) ? "N/A" : SoftBasic.ByteToHexString(bufferR)) + "\n");
 
                             //////////////////////////////////////数据解析，在界面上显示结果，并存储到数据库
                             string showResult = null;//界面显示的结果
